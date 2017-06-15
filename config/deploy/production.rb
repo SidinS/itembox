e7554a83364b58202977fad30789071b88c79e76
Dmitry Shilin <sashsid@tut.by>
2017-06-15 06:05:32 +0300

    turned on confirmation letters

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,6 @@ server '13.58.174.115', user: 'ec2-user', roles: %w(app db web)
 set :deploy_to,  "/var/www/sites/#{fetch :application}"
 set :backup_to, "/var/www/backups/#{fetch(:application)}"
 
-set :branch, ENV['BRANCH'].to_s.empty? ? 'production' : ENV['BRANCH'].to_s
+set :branch, 'production'
 
 set :whenever_environment, 'production'
